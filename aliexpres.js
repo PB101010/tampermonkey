2fb32a033946871d040a76bbdb986151aa2b6668
Ebal Huang <hj101010@live.cn>
2022-08-31 11:04:24 +0800

    add aleixress

---
+++ aliexpres.js
@@ -0,0 +1,32 @@
+function parse_item_id(raw_url){
+    // 解析产品id
+    return /(?<=item\/)\d+/.exec(raw_url);
+}
+
+function product_detail(){
+    let pro_id = parse_item_id(document.activeElement.href);
+    console.log(pro_id);
+    if (pro_id == null){return}
+    let link_ = "https://www.aliexpress.com/item/" + pro_id + ".html";
+    GM_openInTab(link_);
+}
+
+
+function product_edit(){
+    let pro_id = parse_item_id(document.activeElement.href);
+    console.log(pro_id);
+    if (pro_id == null){return}
+    let link_ = "https://gsp.aliexpress.com/apps/product/publish?productId=" + pro_id;
+    GM_openInTab(link_);
+}
+
+
+function ali_order() {
+    GM_openInTab("https://gsp.aliexpress.com/apps/order/detail?orderId=" + document.getSelection());
+}
+
+function aliexpress_main() {
+	GM_registerMenuCommand("订单详情", ali_order, "o");
+    GM_registerMenuCommand("产品链接", product_detail, "p");
+    GM_registerMenuCommand("产品编辑", product_edit, "e");
+}
