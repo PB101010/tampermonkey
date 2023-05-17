e40ddc2bad0e3399541a8e1d94239e1a884fc011
PB101010 <62386582+PB101010@users.noreply.github.com>
2023-05-17 10:41:42 +0800

    Update and rename aliexpres.js to aliexpress_product_id_extractor.user.js

---
+++ aliexpress_product_id_extractor.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @updateURL      https://raw.githubusercontent.com/PB101010/tampermonkey/main/aliexpress_product_id_extractor.user.js
+// @downloadURL    https://raw.githubusercontent.com/PB101010/tampermonkey/aliexpress_product_id_extractor.user.js
+// @name         提取Aliexpress产品ID
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  在浏览器中右键点击链接，提取产品ID，并在新标签页打开
+// @author       Your Name
+// @include      *://*/*
+// @grant        GM_openInTab
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function extractProductId(url) {
+        const regex = /https:\/\/aliexpress\..+\/item\/(\d+)\.html/;
+        const match = url.match(regex);
+        if (match && match[1]) {
+            return match[1];
+        }
+        return null;
+    }
+
+    function openProductPage(productId) {
+        const newUrl = `https://aliexpress.com/item/${productId}.html`;
+        GM_openInTab(newUrl, {active: true});
+    }
+
+    $(document).on('contextmenu', 'a[href*="item/"]', function(event) {
+        event.preventDefault();
+        const productId = extractProductId(this.href);
+        if (productId) {
+            openProductPage(productId);
+        }
+    });
+})();
