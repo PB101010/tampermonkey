c66be4ea5577c3747364c717128a9806ce41f336
PB101010 <62386582+PB101010@users.noreply.github.com>
2023-05-22 14:53:48 +0800

    Update aliexpress.js

--- aliexpress.js
+++ aliexpress.js
@@ -7,10 +7,8 @@
 // @description  在浏览器中右键点击链接，提取产品ID，并在新标签页打开
 // @author       ebal.huangjun@gmail.com
 // @include      https://csp.aliexpress.com/apps/csp/*
-// @include      https://www.aliexpress.com/item/*
 // @grant        GM_openInTab
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
-// @run-at       context-menu
 // ==/UserScript==
 
 (function() {
@@ -23,14 +21,6 @@
             return match[1];
         }
         return null;
-    }
-    
-    function() {
-    'use strict';
-
-    var productId = window.location.href.match(/\d+.html/)[0].replace('.html', '');
-    var newUrl = "https://csp.aliexpress.com/apps/product/publish?productId=" + productId;
-    window.open(newUrl, '_blank');
     }
 
     function openProductPage(productId) {
